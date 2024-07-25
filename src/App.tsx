6cbf6c0b1ab5019c434bac6a96bd68c3b820410f
Kinnouts <kinnouts@gmail.com>
2024-07-25 18:56:41 -0300

    ejercicio 1 empezado

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Info</h1>
+      <h1>Vite + React + Info</h1> /*Esto es lo modificado en primera instancia */
+      <h2>Informatorio 2024</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
